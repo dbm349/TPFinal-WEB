18d064528f6b49971258d37d1b10ee17f1320530
dbm349 <62100143+dbm349@users.noreply.github.com>
2020-06-26 15:58:45 -0300

    Update busqueda.php
    agrego paginado

--- app/views/busqueda.php
+++ app/views/busqueda.php
@@ -36,10 +36,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-    {% block head %}
         <meta charset="utf-8">
-        <title>{% block title %}{% endblock %} - Paginación</title>
-    {% endblock %}
+        <title>PropWeb - Búsqueda</title>
 </head>
 <body>
     
@@ -49,31 +47,32 @@
 
         $base=new PDO("mysql:host=localhost; dbname=propiedadesya", "root", "");
 
-        $base->setAtttribute(PDO::ATTR_ERRORMODE, PDO::ERRMODE_EXCEPTION);
+        $base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         $base->exec("SET CHARACTER SET utf8");
 
-        $tamanio_paginas=3;//cantidad de publicaciones en la pagina
+        $tamanio_paginas=3;//cantidad d epublicaciones en la pagina
 
         if(isset($_GET["pagina"])){
 
             if($_GET["pagina"]==1){
 
-                header("Location:index.html");
+               header("Location:index.php");
 
-            }else{
+           }else{
 
-                $pagina=$_GET["pagina"];
+              $pagina=$_GET["pagina"];
 
           
-            }
-        }else{
-            $pagina=1;
+             }
+            }else{
+          
+           $pagina=1;
 
         }
 
         
-        $empezar_desde=($pagina-1)*tamanio_paginas;
+        $empezar_desde=($pagina-1)*$tamanio_paginas;//almacena el registro desde el cual queiro mostrar los resultado
 
         $sql_total="SELECT * FROM propiedades ";
 
@@ -83,29 +82,27 @@
 
         $num_filas=$resultado->rowCount();
 
-        $total_paginas=ceil($num_filas/tamanio_paginas);
+        $total_paginas=ceil($num_filas/$tamanio_paginas);
 
             echo "Cantidad de resultados encontrados: " . $num_filas . "<br>";
             echo "Mostrando la página: " . $pagina . "de ". $total_paginas . "<br>";
 
 
-            $resultado->closeCursor();
+        
+           $sql_limite="SELECT * FROM propiedades LIMIT $empezar_desde, $tamanio_paginas";
 
-            $sql_limite="SELECT * FROM propiedades  LIMIT $empezar_desde, $tamanio_paginas";
+           $resultado=$base->prepare($sql_limite);
 
-            $resultado=$base->prepare($sql_limite);
-
-            $resultado->execute(array());
-
+           $resultado->execute(array());
 
 
-            while($registro=$resultado->fetch(PDO::FETCH_ASSOC)){
+             while($registro=$resultado->fetch(PDO::FETCH_ASSOC)){
 
-                echo "Tipo propiedad: " . $registro["tipo_propiedad"];
+                echo "Operacion: " .$registro["tipo_operacion"]. "  Tipo:  " . $registro["tipo_propiedad"]. "<br>";
                 /*MOSTRAR DATOS DE LA PROPIEDAD*/
-            }
+            } 
 
-
+             $resultado->closeCursor();
       }catch (Exception $e){
         echo "Linea de error:" . $e->getLine();
 
@@ -114,11 +111,13 @@
 
 //--------------------------------PAGINACION----------------------------------------------//
 
-      for($i=1; $i<=$total_paginas; $i++){
+     for($i=1; $i<=$total_paginas; $i++){
 
         echo "<a href='?pagina="  . $i . "'>" . $i . "</a> ";
       }
 
     ?>
+
+
 </body>
 </html>
